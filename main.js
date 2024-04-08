44bff37c45b8830bfeab99d260f84fb63d6635ab
kariomart <mnn264@nyu.edu>
2024-04-08 19:11:15 -0400

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+const imageContainer = document.querySelector(".gallery"); 
+const images = document.querySelectorAll(".gallery")
+
+// const updateGalleryItems = () => {
+//     images.forEach((item)) => {
+//         if (item.isHovered) {
+//             //do stuff here
+//         }
+//     })
+// }
