2bab839451488c187f0cbb2e5c2618dfb8a4f067
kariomart <mnn264@nyu.edu>
2024-04-19 16:30:45 -0400

    small fixes

--- floater.js
+++ floater.js
@@ -1,11 +1,15 @@
 window.addEventListener('scroll', function() {
-    const stickyText = document.querySelector('.floater');
+    const stickyText = document.querySelectorAll('.floater');
     const contentTop = document.querySelector('.main').offsetTop;
     const scrollPosition = window.scrollY;
     console.log(scrollPosition);
-    if (scrollPosition >= 866) {
-      stickyText.classList.add('sticky');
-    } else {
-      stickyText.classList.remove('sticky');
+    if (window.innerWidth >= 400) {
+      if (scrollPosition >= 866) {
+        stickyText[0].style.position = 'fixed';
+        stickyText[0].style.top = '5vh';
+      } else {
+        stickyText[0].style.position = 'absolute';
+        stickyText[0].style.top = '90vh';
+      }
     }
   });
